4cc12180c9d8d44aabcb19601a5a9b7ddf949070
Brian Danielak <brian@briandk.com>
2015-11-28 22:59:08 -0500

    Add installation instructions

--- install-academic-writing-with-vagrant.sh
+++ install-academic-writing-with-vagrant.sh
@@ -1,16 +1,6 @@
-# academic-writing-with-vagrant
-An entire vagrant VM for portably compiling RMarkdown files. Useful for when you need to collaborate over a manuscript and you want to make sure everyone has a dependency-managed way to compile the manuscript.
-
-# Installation
-
-Copy and paste this into your terminal, then hit Enter
-
-```bash
 curl -O https://cdn.rawgit.com/briandk/academic-writing-with-vagrant/master/makefile
 curl -O https://cdn.rawgit.com/briandk/academic-writing-with-vagrant/master/Vagrantfile
 curl -O https://cdn.rawgit.com/briandk/academic-writing-with-vagrant/master/render_manuscript.R
 curl -O https://cdn.rawgit.com/briandk/academic-writing-with-vagrant/master/r-dependencies.R
 curl -O https://cdn.rawgit.com/briandk/academic-writing-with-vagrant/master/bootstrap.sh
 vagrant up
-
-```
