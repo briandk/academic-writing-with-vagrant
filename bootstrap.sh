ec884093a69a8b61c2ea313795f05da7664d76b8
Brian Danielak <brian@briandk.com>
2015-12-17 17:57:49 -0500

    Try using RIT as a faster mirror

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,9 @@ echo "DATE ||| $(date)"
 # Variables
 RSTUDIOVERSION='rstudio-server-0.99.824-amd64.deb'
 
+# Try replacing the standard ubuntu archive with a faster mirror
+sed -i.bak 's/archive.ubuntu.com/mirrors.rit.edu/' /etc/apt/sources.list
+
 # Add CRAN mirror to apt-get sources
 add-apt-repository "deb https://cran.rstudio.com/bin/linux/ubuntu trusty/"
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
